eda29edbb77dab330197ca0c9993a886b8867c6f
avfonseca <afonseca@ccom.unh.edu>
2023-09-22 14:44:43 -0400

    Rewrote dataset so random shuffler can now choose a random fragment from a survey

--- data.py
+++ data.py
@@ -5,9 +5,10 @@ import pyarrow.dataset as ds
 import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset
+import random
 
 class HydroNet(Dataset):
-    def __init__(self, num_points, survey_list, resolution, partition ='train', mode = 'autoencode', simple_load = True, col_list = None):
+    def __init__(self, num_points, survey_list, resolution, partition ='train', simple_load = True, col_list = None):
         
         #num_points is the number of points to construct the graph 
         self.BASE_DIR = "."
@@ -18,98 +19,66 @@ class HydroNet(Dataset):
         self.num_points = num_points
         self.partition = partition 
         self.resolution = resolution 
-        self.mode = mode
-
-        self.data = []
+        
         self.pc_tree = []
         self.pc = []
-        self.fragment_length = []
-        self.fragment_sum = []
+        print(len(survey_list))
+        self.data = [[]]*len(survey_list)
+        print(len(self.data))
+        self.fragment_length = [[]]*len(survey_list)
+        self.survey_length = []
 
         for i in range(len(survey_list)):
             
+            if (self.partition == "test"):
+                dataset = ds.dataset(os.path.join(self.BASE_DIR, 'data', survey_list[i], "parquet", "rej_" + survey_list[i] + ".parquet"), format="parquet", partitioning="hive")
+                iterable = list(dataset.get_fragments())
             
-            dataset_acc = ds.dataset(os.path.join(self.BASE_DIR, 'data', survey_list[i], "parquet", "acc_" + survey_list[i] + ".parquet"), format="parquet", partitioning="hive")
-            
-            iterable_acc = list(dataset_acc.get_fragments())
-            
-            
-            num_acc , num_rej = 0,0
-            lengths_acc = []
-            lengths_rej = []
-            
-            
-            for fragment in iterable_acc:
-                tb = fragment.to_table(columns = ["Flag"])
-                lengths_acc.append(tb.num_rows)
-                num_acc += tb.num_rows
-                
-            if (partition == "test"):
-                dataset_rej = ds.dataset(os.path.join(self.BASE_DIR, 'data', survey_list[i], "parquet", "rej_" + survey_list[i] + ".parquet"), format="parquet", partitioning="hive")
-                iterable_rej = list(dataset_rej.get_fragments())
+            else:
+                dataset = ds.dataset(os.path.join(self.BASE_DIR, 'data', survey_list[i], "parquet", "acc_" + survey_list[i] + ".parquet"), format="parquet", partitioning="hive")
+                iterable = list(dataset.get_fragments())
 
-                for fragment in iterable_rej:
+            for fragment in iterable:
                     tb = fragment.to_table(columns = ["Flag"])
-                    lengths_rej.append(tb.num_rows)
-                    num_rej += tb.num_rows
-                
-            
-            
-            if (self.mode == "balance"):
-            #balance data_sets  (repeating least represented class) 
-                self.data.append(np.hstack((np.repeat(iterable_acc,max(1,int(num_rej/num_acc))), np.repeat(iterable_rej,max(1,int(num_acc/num_rej))))))
-                self.fragment_length.append(np.hstack((np.repeat(lengths_acc,max(1,int(num_rej/num_acc))), np.repeat(lengths_rej,max(1,int(num_acc/num_rej))))))
-            else:   
-                if (self.partition == 'test'):
-                    self.data.append(iterable_rej)
-                    self.fragment_length.append(lengths_rej)
-                else:
-                    self.data.append(iterable_acc)
-                    self.fragment_length.append(lengths_acc)
+                    length = tb.num_rows
+                    self.data[i].append(iterable)
+                    self.fragment_length[i].append(length)
            
-           
-            self.fragment_sum.append(np.cumsum(self.fragment_length[i]))
-                
+               
             self.load_pointcloud(survey_list[i],self.resolution)
-            
+            self.survey_length.append(len(self.fragment_length[i]))
         
-        self.len_survey = np.array(list(map(lambda x: sum(x), self.fragment_length)))
-        self.survey_sum = np.cumsum(self.len_survey)
-
+        self.dataset_length = sum(self.survey_length)
 
     def __getitem__(self, item):
         
-         # Continously get new item from different surveys if
-         # ex if survey 1 has 100 points, 101 will be the first point of survey 2
-        
-        survey = sum(item > self.survey_sum - 1)
-        item =  item - np.insert(self.survey_sum,0,0)[survey]
-        
-        fragment = sum(item > self.fragment_sum[survey] - 1)
-        item = item - np.insert(self.fragment_sum[survey],0,0)[fragment]
-
+         # Randomly get an item from selected fragment
+        survey = sum(item > self.survey_length - 1)
+        fragment = item - np.insert(self.survey_length,0,0)[survey]
         fragment_data, fragment_label = self.load_data(self.data[survey][fragment])
         
         
-        point = fragment_data[item,:]
-        label = fragment_label[item,:]
+        choice = random.randint(0, fragment_data.shape[0])
+        
+        point = fragment_data[choice,:]
+        label = fragment_label[choice,:]
         
         
         #right now I still havent implemented the multiple resolutions
         tree = self.pc_tree[survey][self.resolution[0]][0]
         mean = self.pc_tree[survey][self.resolution[0]][1]
-        var = self.pc_tree[survey][self.resolution[0]][2]
+        cov = self.pc_tree[survey][self.resolution[0]][2]
         
         [_, idx, _] = tree.search_hybrid_vector_3d(point,self.num_points,100)
         
         nn = np.asarray(self.pc[survey][self.resolution[0]].points)[idx]
               
-        return (np.concatenate((nn,point.reshape(1,-1))) - mean)/np.diagonal(var),label
+        return (np.concatenate((nn,point.reshape(1,-1))) - mean)/np.diagonal(cov),label
         
          
 
     def __len__(self):
-         return sum(self.len_survey)
+         return self.dataset_length
      
     
     def load_pointcloud(self,survey, res_list):
